962aad5abc181f06b1b895385c8e54cf1de61cd4
philip <philip.clarke@redhood.co.uk>
2016-05-02 15:40:23 +0100

    ready to play with product methods

--- client/app/products/products.service.js
+++ client/app/products/products.service.js
@@ -3,11 +3,11 @@
 angular.module('beer2App')
   .factory('Product', function () {
     return [
-       {id: 1, title: 'Product 1', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
-       {id: 2, title: 'Product 2', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
-       {id: 3, title: 'Product 3', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
-       {id: 4, title: 'Product 4', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
-       {id: 5, title: 'Product 5', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'}
+       {id: 1, title: 'Beer 1', price: 2.45, quantity: 10, description: 'Great Beer'},
+       {id: 2, title: 'Beer 2', price: 3.45, quantity: 10, description: 'Fantastic Beere'},
+       {id: 3, title: 'Beer 3', price: 1.45, quantity: 10, description: 'Wonderful beer'},
+       {id: 4, title: 'Beer 4', price: 1.45, quantity: 10, description: 'Super Beer'},
+       {id: 5, title: 'Beer 5', price: 3.45, quantity: 10, description: 'Lovely Beer'}
      ];
 
   });
